e4073f60bacbd8f9aa011ce8a9c1de3939493ca0
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-09 06:21:29 +0100

    add country-list component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { CountryComponent } from './country/country.component';
 import { CountriesComponent } from './countries.component';
+import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
   declarations: [
     CountryComponent,
-    CountriesComponent
+    CountriesComponent,
+    CountryListComponent
   ],
   imports: [CommonModule, CountriesRoutingModule],
 })
---
+++ src/app/countries/country-list/country-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rc-country-list',
+  templateUrl: './country-list.component.html',
+  styles: [
+  ]
+})
+export class CountryListComponent {
+
+}
