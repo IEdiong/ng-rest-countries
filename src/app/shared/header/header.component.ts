87156915436ba3695a080792aca1e8d4ac6b5ed9
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-09 10:11:10 +0100

    implement theme toggle

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { ITheme } from '../interfaces/header.model';
 
 @Component({
   selector: 'rc-header',
   templateUrl: './header.component.html',
   styles: [],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  theme: ITheme = 'light';
+
+  toggleTheme(): void {
+    // Toggle the theme
+    if (this.theme === 'light') {
+      this.theme = 'dark';
+      localStorage['theme'] = 'dark';
+    } else {
+      this.theme = 'light';
+      localStorage['theme'] = 'light';
+    }
+
+    const htmlElement = document.documentElement;
+    htmlElement.classList.toggle('dark');
+  }
+
+  ngOnInit(): void {
+    // Set theme based on user's preferences
+    if (
+      localStorage['theme'] === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark');
+      this.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      this.theme = 'light';
+    }
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -10,6 +11,7 @@ module.exports = {
         "v-light-gray": "hsl(0, 0%, 98%)", // Light Mode Background
 
         // dark mode
+        "lighter-blue": "hsl(212, 25%, 24%)", // Dark Mode Elements Hover
         "dark-blue": "hsl(209, 23%, 22%)", // Dark Mode Elements
         "vd-dark-blue": "hsl(207, 26%, 17%)", // Dark Mode Background
       },
