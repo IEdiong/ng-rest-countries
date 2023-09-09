cea723c49c80d5fc933037f2785931f2415f6b7e
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-09 05:56:41 +0100

    add countries feature

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CountriesRoutingModule } from './countries-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CountriesRoutingModule],
+})
+export class CountriesModule {}
