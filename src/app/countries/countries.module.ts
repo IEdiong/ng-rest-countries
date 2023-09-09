80ab5e8d183ce8a7e7f1c29bab0d7cb9597084c6
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-09 06:03:50 +0100

    add country

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CountriesRoutingModule } from './countries-routing.module';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CountryComponent
+  ],
   imports: [CommonModule, CountriesRoutingModule],
 })
 export class CountriesModule {}
---
+++ src/app/countries/country/country.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rc-country',
+  templateUrl: './country.component.html',
+  styles: [
+  ]
+})
+export class CountryComponent {
+
+}
