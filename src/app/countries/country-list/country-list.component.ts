67b3b15f7199fe360670f5e5b75c2d73db2a286e
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-10-16 00:52:17 +0100

    add procedural data retrieval pattern in country-list

--- src/app/countries/country-list/country-list.component.ts
+++ src/app/countries/country-list/country-list.component.ts
@@ -1,46 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ICountry } from '../shared/country.model';
 import { CountryService } from '../shared/country.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'rc-country-list',
   templateUrl: './country-list.component.html',
-  styles: [],
+  styles: [
+    `
+      // THE LOADING EFFECT
+      .skeleton {
+        background-color: #e2e5e7;
+        // The shine that's going to move across the skeleton:
+        background-image: linear-gradient(
+          90deg,
+          rgba(#fff, 0),
+          rgba(#fff, 0.5),
+          rgba(#fff, 0)
+        );
+        background-size: 40px 100%; // width of the shine
+        background-repeat: no-repeat; // No need to repeat the shine effect
+        background-position: left -40px top 0; // Place shine on the left side, with offset on the left based on the width of the shine - see background-size
+        animation: shine 1s ease infinite; // increase animation time to see effect in 'slow-mo'
+      }
+
+      @keyframes shine {
+        to {
+          // Move shine from left to right, with offset on the right based on the width of the shine - see background-size
+          background-position: right -40px top 0;
+        }
+      }
+    `,
+  ],
 })
 export class CountryListComponent implements OnInit {
-  countries: ICountry[] | undefined;
-  isLoading: boolean = false;
+  countries$: Observable<ICountry[]> | undefined;
+  isLoading: boolean = true;
   hasError: boolean = false;
 
   constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {
-    this.countryService.getCountries().subscribe({
-      next: (data) => {
-        console.log(data);
-        let favArr: any = [];
-
-        if (data) {
-          ['Germany', 'United States of America', 'Brazil', 'Iceland'].forEach(
-            (el) => {
-              let tempData = data.find((cty) => cty.name === el);
-              favArr.push(tempData);
-              data = data.filter((cty) => cty.name !== el);
-            }
-          );
-
-          setTimeout(() => {
-            this.countries = [...favArr, ...data];
-          }, 2000);
-        }
-        this.hasError = false;
-        this.isLoading = false;
-      },
-      error: (error) => {
-        this.hasError = true;
-        this.isLoading = false;
-        this.countries = [];
-      },
-    });
+    this.countries$ = this.countryService.getCountries();
+    this.isLoading = false;
   }
 }
--- src/app/countries/shared/country.service.ts
+++ src/app/countries/shared/country.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ICountry } from './country.model';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,19 @@ export class CountryService {
   constructor(private http: HttpClient) {}
 
   getCountries() {
-    return this.http.get<ICountry[]>(this.url);
+    return this.http.get<ICountry[]>(this.url).pipe(
+      map((data) => {
+        let favArr: ICountry[] = [];
+        ['Germany', 'United States of America', 'Brazil', 'Iceland'].forEach(
+          (el) => {
+            let tempData = data.find((cty) => cty.name === el);
+            if (tempData !== undefined) favArr.push(tempData);
+            data = data.filter((cty) => cty.name !== el);
+          }
+        );
+        return [...favArr, ...data];
+      })
+    );
   }
 
   getCountry(name: string) {
