58c6651e012f103b2567c0b3912d7daeba0f8163
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-02-13 15:31:14 +0100

    add styles for filter button

---
+++ src/app/shared/filter/filter.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rc-filter',
+  templateUrl: './filter.component.html',
+  styles: [],
+})
+export class FilterComponent implements OnInit {
+  regions: string[] = [];
+  menuIsOpen: boolean = true;
+
+  ngOnInit(): void {
+    this.regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  }
+
+  toggleMenu() {
+    this.menuIsOpen = !this.menuIsOpen;
+    console.log('menu is toggled');
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
       colors: {
         // light mode
-        "v-dark-blue": "hsl(200, 15%, 8%)", // Light Mode Text
+        "v-dark-blue": "hsl(200, 15%, 8%)", //? Light Mode Text (#111517)
         "dark-gray": "hsl(0, 0%, 52%)", // Light Mode Input
         "v-light-gray": "hsl(0, 0%, 98%)", // Light Mode Background
 
@@ -21,6 +21,7 @@ module.exports = {
         back: "0px 0px 7px 0px rgba(0, 0, 0, 0.29)",
         flag: "0px 0px 14px 4px rgba(0, 0, 0, 0.03)",
         tag: "0px 0px 4px 1px rgba(0, 0, 0, 0.10)",
+        filter: "0px 2px 9px 0px rgba(0, 0, 0, 0.05)",
       },
       container: {
         center: true,
